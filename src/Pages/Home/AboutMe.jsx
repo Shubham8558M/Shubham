a7c38626af03cdb3939cfa130b71871fe6d547f3
Shubham8558M <112143453+Shubham8558M@users.noreply.github.com>
2024-05-07 22:06:16 +0530

    Commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hello there! I'm Shubham, a passionate fullstack developer with a
+            keen eye for crafting immersive digital experiences. With a blend of
+            creativity and technical prowess, I thrive on bringing ideas to life
+            across both frontend and backend realms.
+          </p>
+          <p className="hero--section-description">
+            My journey in the world of development is marked by a relentless
+            pursuit of excellence, fueled by a curiosity to explore the latest
+            technologies and methodologies. Let's innovate and build together
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Shubham</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Hey, I'm Shubham, a skilled developer driven by innovation and
+            excellence in software engineering.
+            <br /> Let's build something incredible together
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/Shubham.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
